ccac8574616ba81bf32f6ba4cf43e009d9d2c112
GerryTo <myevocreo@gmail.com>
2024-01-05 18:02:12 +0700

    testing

---
+++ src/layouts/mybank-datalist/data/dataListTableData.jsx
@@ -0,0 +1,49 @@
+import { useEffect, useState } from 'react'
+import { apiGetMybankList } from '../../../services/api'
+
+const dataListTableData = () => {
+  const [records, setRecords] = useState({})
+  const [isLoading, setIsloading] = useState(false)
+
+  useEffect(() => {
+    getList()
+  }, [])
+
+  async function getList() {
+    try {
+      setIsloading(true)
+      const { data } = await apiGetMybankList()
+      if (data.status === 'ok') {
+        setRecords(data.records)
+        setIsloading(false)
+      } else {
+        alert('error')
+        setIsloading(false)
+      }
+    } catch (e) {
+      setIsloading(false)
+      console.log(e)
+    }
+  }
+
+  let tmpDataColumn = [
+    {
+      title: 'id',
+      dataIndex: 'n_id',
+      key: 'n_id'
+    },
+    {
+      title: 'status',
+      dataIndex: 'v_status',
+      key: 'v_status'
+    }
+  ]
+
+  return {
+    column: tmpDataColumn,
+    records: records,
+    isLoading
+  }
+}
+
+export default dataListTableData
