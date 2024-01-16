eb83584b140ac99577823f1c9407d8b60ae3ebe2
Excell <90251591+Beatwice@users.noreply.github.com>
2024-01-16 12:37:17 +0700

    dash board

--- src/layouts/Dashboard.jsx
+++ src/layouts/Dashboard.jsx
@@ -1,5 +1,39 @@
+import { Card, Col, DatePicker, Row, Space, Typography } from 'antd';
 const Dashboard = () => {
-  return <div>Dashboard</div>;
+  const { Title } = Typography;
+  return(
+    <>
+      <Title level={4}>Dashboard</Title>
+      <div style={{paddingBottom: "1%"}}>
+        <Space>
+          <DatePicker onChange="" />
+          <DatePicker onChange="" />
+        </Space>
+      </div>
+      <Row gutter={16}>
+        <Col span={6}>
+          <Card title="Total Deposit" bordered={false}>
+            RM
+          </Card>
+        </Col>
+        <Col span={6}>
+          <Card title="Total Withdraw" bordered={false}>
+            RM
+          </Card>
+        </Col>
+        <Col span={6}>
+          <Card title="Total Deposit Fee" bordered={false}>
+            RM
+          </Card>
+        </Col>
+        <Col span={6}>
+          <Card title="Total Deposit Fee" bordered={false}>
+            RM
+          </Card>
+        </Col>
+      </Row>
+    </>
+  )
 };
 
 export default Dashboard;
